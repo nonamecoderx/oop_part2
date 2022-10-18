24c4aa5d4c24f726e00db934fc5488e4eb9982aa
Иван <ivankiseliov@rambler.ru>
2022-10-18 21:56:49 +0300

    dz3

--- src/Human.java
+++ src/Human.java
@@ -4,21 +4,26 @@ public class Human {
     String town;
     String job;
 
-    Human(int yearOfBirth) {
-        this.yearOfBirth = yearOfBirth;
-        name = "Информация не указана";
-        town = "Информация не указана";
-        job = "Информация не указана";
-    }
-
     Human(int yearOfBirth, String name, String town, String job) {
         if (yearOfBirth < 0) {
             System.out.println("Год рождения не может быть отрицательным");
         } else {
             this.yearOfBirth = yearOfBirth;
         }
-        this.name = name;
-        this.town = town;
-        this.job = job;
+        if (name.isEmpty() ){
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+        if (town.isEmpty() ){
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+        if (job.isEmpty() ){
+            this.job = "Информация не указана";
+        } else {
+            this.job = job;
+        }
     }
 }
