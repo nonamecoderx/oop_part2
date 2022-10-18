a18fe47934dbee466903110232dbc98f53b823b6
Иван <ivankiseliov@rambler.ru>
2022-10-18 21:32:55 +0300

    dz3

--- src/Human.java
+++ src/Human.java
@@ -3,8 +3,20 @@ public class Human {
     String name;
     String town;
     String job;
-    Human (int yearOfBirth, String name, String town, String job) {
+
+    Human(int yearOfBirth) {
         this.yearOfBirth = yearOfBirth;
+        name = "Информация не указана";
+        town = "Информация не указана";
+        job = "Информация не указана";
+    }
+
+    Human(int yearOfBirth, String name, String town, String job) {
+        if (yearOfBirth < 0) {
+            System.out.println("Год рождения не может быть отрицательным");
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
         this.name = name;
         this.town = town;
         this.job = job;
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Human human1 = new Human(1987,"Максим", "Минск", "бренд-менеджер");
-        Human human2 = new Human(1993,"Аня","Москва","методист образовательных программ");
-        Human human3 = new Human(1994,"Катя","Калининград","продакт-менеджер");
-        Human human4 = new Human(1995,"Артем","Москва","директор по развитию бизнеса");
+        Human human1 = new Human(1987, "Максим", "Минск", "бренд-менеджер");
+        Human human2 = new Human(1992, "Аня", "Москва", "методист образовательных программ");
+        Human human3 = new Human(1994, "Катя", "Калининград", "продакт-менеджер");
+        Human human4 = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
         System.out.println("Привет! Меня зовут " + human1.name + ". Я из города " + human1.town + ". Я родился в " + human1.yearOfBirth + " году. Я работаю на должности " + human1.job + ". Будем знакомы!");
         System.out.println("Привет! Меня зовут " + human2.name + ". Я из города " + human2.town + ". Я родилась в " + human2.yearOfBirth + " году. Я работаю на должности " + human2.job + ". Будем знакомы!");
         System.out.println("Привет! Меня зовут " + human3.name + ". Я из города " + human3.town + ". Я родилась в " + human3.yearOfBirth + " году. Я работаю на должности " + human3.job + ". Будем знакомы!");
